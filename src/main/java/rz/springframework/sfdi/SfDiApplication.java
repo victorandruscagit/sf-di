69fced86246de6c451bf224a3218cebd452cfa9a
victorandruscabit <victor.v.andrusca@gmail.com>
2021-03-15 09:26:57 +0200

    setup

---
+++ src/main/java/rz/springframework/sfdi/SfDiApplication.java
@@ -0,0 +1,13 @@
+package rz.springframework.sfdi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfDiApplication.class, args);
+	}
+
+}
