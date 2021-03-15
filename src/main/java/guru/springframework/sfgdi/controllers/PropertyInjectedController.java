b4bd05b360d72c4b3f0338a4ae5ea15eebf197d4
victorandruscabit <victor.v.andrusca@gmail.com>
2021-03-15 16:27:32 +0200

    primary beans

--- src/main/java/guru/springframework/sfgdi/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PropertyInjectedController.java
@@ -2,6 +2,7 @@ package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 
@@ -11,7 +12,9 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class PropertyInjectedController {
 
+
     @Autowired
+    @Qualifier("propertyInjectedGreetingService")
     public GreetingService greetingService;
 
     public String getGreeting(){
--- src/main/java/guru/springframework/sfgdi/services/ConstructorGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/ConstructorGreetingService.java
@@ -1,5 +1,6 @@
 package guru.springframework.sfgdi.services;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
@@ -7,10 +8,11 @@ import org.springframework.stereotype.Service;
 /**
  * Created by jt on 12/26/19.
  */
+@Primary
 @Service
 public class ConstructorGreetingService implements GreetingService {
     @Override
     public String sayGreeting() {
-        return "Hello World";
+        return "Hello World -  From the PRIMARY bean";
     }
 }
