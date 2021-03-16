4c195427e8a05783ac91f39b98a1e159c6421f4d
victorandruscabit <victor.v.andrusca@gmail.com>
2021-03-16 09:35:51 +0200

    profiles-active and default

---
+++ src/main/java/guru/springframework/sfgdi/services/L18nEnglishGreetingService.java
@@ -0,0 +1,16 @@
+package guru.springframework.sfgdi.services;
+
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Profile("EN")
+@Service("i18nService")
+public class L18nEnglishGreetingService   implements  GreetingService{
+
+    @Override
+    public String sayGreeting() {
+        return "hello world --- EN";
+
+    }
+}
