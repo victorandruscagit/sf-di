1ed77bfc2d358c81572fd92e0f41432e5187a571
victorandruscagit <victor.v.andrusca@gmail.com>
2021-04-10 19:14:00 +0300

    using faotory beans

--- sf-di-dev-qualifiers/src/main/java/guru/springframework/sfgdi/config/GrretinServiceConfig.java
+++ sf-di-dev-qualifiers/src/main/java/guru/springframework/sfgdi/config/GrretinServiceConfig.java
@@ -1,26 +1,52 @@
 package guru.springframework.sfgdi.config;
 
+import com.springframework.pets.DogPetService;
+import com.springframework.pets.PetService;
+import com.springframework.pets.PetServiceFactory;
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepositoryImpl;
 import guru.springframework.sfgdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
 
 @Configuration
 public class GrretinServiceConfig {
 
 
+    @Bean
+    PetServiceFactory petServiceFactory(){
+        return  new PetServiceFactory();
+    }
+
+    @Profile({"dog", "default"})
+    @Bean
+    PetService dogPetService(PetServiceFactory petServiceFactory){
+         return  petServiceFactory.getPetService("dog");
+
+    }
+    @Profile("cat")
+    @Bean
+    PetService catPetService(PetServiceFactory petServiceFactory){
+         return  petServiceFactory.getPetService("cat");
+
+    }
+    @Bean
+    EnglishGreetingRepository englishGreetingRepository(){
+        return new EnglishGreetingRepositoryImpl();
+    }
 
     @Bean("i18nService")
     @Profile({"ES", "default"})
     I18NSpanishService i18NSpanishService(){
         return  new I18NSpanishService();
     }
+
     @Profile("EN")
     @Bean
-    I18nEnglishGreetingService i18nService(){
-        return new I18nEnglishGreetingService();
+    I18nEnglishGreetingService i18nService(EnglishGreetingRepository englishGreetingRepository){
+        return new I18nEnglishGreetingService(englishGreetingRepository);
     }
 
 
